095a41c3706126d2ad971e47a8ee267d4bc420d3
Natasha Fray <natasha.fray9@gmail.com>
2020-12-22 10:33:20 -0500

    making changes to see the app auto deploy ;)

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 const express = require('express');
 
 const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes');
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -13,7 +12,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 // Tells node that we are creating an "express" server
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port used later in the listener
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
